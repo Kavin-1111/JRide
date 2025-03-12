6c4d5abb490de2f764b8878fa3faf42e089e9697
kavin-1111 <kavin.n@jmangroup.com>
2025-03-12 15:43:59 +0530

    back-end removed

--- Back-End/routes/driverRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { registerDriver, getAllDrivers } = require('../controllers/driverController');
-const authMiddleware = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.post('/register', authMiddleware, registerDriver);
-router.get('/', getAllDrivers);
-
-module.exports = router;
